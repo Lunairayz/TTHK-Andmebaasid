244a56fe47fdf023a75b0d659968358abd91a4c9
Lunairayz <archieshirjaev@gmail.com>
2024-04-15 13:00:30 +0300

    teatr

--- teatr.sql
+++ teatr.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE TeatrSirjajev;
+Use TeatrSirjajev;
+
+CREATE TABLE filmType(
+	filmTypeID int Primary key identity (1,1),
+	filmType varchar(25),
+	kirjeldus TEXT);
+select * from filmType;
+INSERT INTO filmType (filmType, kirjeldus)
+VALUES ('Sipsik', 'vaike sinine poiss');
