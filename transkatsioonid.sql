e6881c63a7d040902ecb71147143410548a09642
Lunairayz <archieshirjaev@gmail.com>
2024-05-08 13:49:59 +0300

    print tabeli Person andmed ja kustuta kellel on ID 8

--- transkatsioonid.sql
+++ transkatsioonid.sql
@@ -89,7 +89,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
 
---?
+-- print tabeli Person andmed ja kustuta kellel on ID 8.
 select * from Person
 go
 delete from Person where Id = 8
